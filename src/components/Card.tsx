10607a27fb645fdf90eac143cb6345ea7eff1d5e
Omid-Neshati <tchandnofame@gmail.com>
2022-06-01 10:12:24 +0330

    add card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,9 +1,11 @@
+import Image from "next/image";
 import React, { FC } from "react";
 import TCard from "../types/card";
 const Card: FC<TCard> = ({ id, icon }) => {
   return (
-    <div className="flex items-center justify-center w-20 h-20 p-2 bg-gray-200 rounded-md aspect-square">
-      <svg href="" className="w-full h-full"></svg>
+    <div className="flex items-center justify-center p-2 bg-gray-200 rounded-md aspect-square">
+      {/* <svg name={id} ={icon} className="w-full h-full"></svg> */}
+      <Image src={icon} width={"100%"} height={"100%"} alt={id} />
     </div>
   );
 };
